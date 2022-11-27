d31baadb321b7e7a2ff59907db5351eda23c37c0
devripon <you@example.com>
2022-11-27 23:01:58 +0600

    frontend design complete

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+const clock = document.querySelector('.clock');
+const start = document.querySelector('start');
+const stop = document.querySelector('stop');
+const lap = document.querySelector('lap');
+const reset = document.querySelector('reset');
